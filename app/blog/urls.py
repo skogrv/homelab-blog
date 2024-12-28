63137a9aea59d21a541671a51d1d3d225bee64ab
Ihor Skohariev <skogrv@gmail.com>
2024-12-28 15:04:54 +0200

    add likes, fix mobile

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import Index, Article, DetailArticleView
+from .views import Index, Article, DetailArticleView, LikeArticle
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('tinymce/', include('tinymce.urls')),
     path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
+    path('<int:pk>/like', LikeArticle.as_view(), name='like_article')
 ]
