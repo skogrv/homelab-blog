9bfbefaab2656795844a87ea9e1fcce4c12079b7
Ihor Skohariev <skogrv@gmail.com>
2025-01-10 20:05:16 +0200

    add healthy check for db

---
+++ app/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$SQL_HOST" = "db" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
